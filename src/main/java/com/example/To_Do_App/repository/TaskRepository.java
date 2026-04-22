33855efb834fb32782b1360e46f2b35f8bc09556
Danil0ss <2005prudnikov@gmail.com>
2026-04-23 01:27:15 +0300

    add methods in repository

--- src/main/java/com/example/To_Do_App/repository/TaskRepository.java
+++ src/main/java/com/example/To_Do_App/repository/TaskRepository.java
@@ -2,10 +2,22 @@ package com.example.To_Do_App.repository;
 
 import com.example.To_Do_App.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
 
     List<Task> findByUserIdOrderByPositionAsc(String userId);
+    @Modifying
+    @Query(value = "SELECT MAX (t.position) FROM Task t WHERE t.userId=:userId")
+    Optional<Integer> findMaxPositionByUserId(@Param("userId") String userId);
+
+    @Modifying
+    @Query(value = "UPDATE Task t SET t.position=t.position-1 "+
+                    "WHERE t.userId=:userId AND t.position>:position")
+    void updateTaskPosition(@Param("userId") String userId,@Param("position") Integer position);
 }
